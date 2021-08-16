7aeafb02294a956c2726d94caa7f2513714fc8e1
ScottCrawley <74011247+Scott-Crawley@users.noreply.github.com>
2021-08-16 13:53:11 +0000

    Fully implemented temperature conversion

--- include/monday.h
+++ include/monday.h
@@ -2,22 +2,48 @@
 
 void fahrenheitCentigradeConversion(void) {
   using namespace std;
-
+ 
   float temp  = 0.0;
+  float conv  = 0.0;
+  string inp  = "0";
   char choice = '0';
 
-	cout << "Please enter the starting temperature: ";
-  cin >> temp;
-  cout << "Press 'C' to convert from Fahrenheit to Centigrade." << endl;
-  cout << "Press 'F' to convert from Centigrade to Fahrenheit." << endl;
-  cin >> choice;
+  // Starting temp + looped numerical validation
+  cout << "Please enter the starting temperature: ";
+  while (true) {
+    try {
+      cin >> inp;
+      temp = stoi(inp);
+      break;
+    } 
+    catch (invalid_argument ignored) {
+      cout << "Please enter a valid integer: ";
+    }
+  }
 
-  if (tolower(choice) == 'c') {
-    cout << "";
-  } else if (tolower(choice) == 'f') {
-    cout << "";
-  } else {
-    cout << "Not a valid option";
+  // Conversion mode and choice evaluation (with case insensitivity)
+  cout << "\nPress 'C' to convert from Fahrenheit to Centigrade.\n";
+  cout << "Press 'F' to convert from Centigrade to Fahrenheit.\n\n";
+  cout << "Your choice: ";
+
+  cin >> choice;
+  choice = tolower(choice);
+  if (choice == 'c') {
+    conv = (temp - 32) * 5 / 9;
+    cout << temp << " degrees Fahrenheit is " << conv << " Centigrade\n";
+    if (conv < -273.15) {
+      cout << "Breaks the laws of physics!\n";
+    }
+  } 
+  else if (choice == 'f') {
+    conv = (temp * 9 / 5) + 32;
+    cout << temp << " degrees Centigrade is " << conv << " Fahrenheit\n";
+    if (conv < -459.67) {
+      cout << "Breaks the laws of physics!\n";
+    }
+  } 
+  else {
+    cout << "Not a valid option\n";
   }
 }
 
