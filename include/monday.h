956969044497cc4ba12ffa9668d56aa6acaa8a83
ScottCrawley <74011247+Scott-Crawley@users.noreply.github.com>
2021-08-16 12:57:53 +0000

    Initial setup of temp-converter

--- include/monday.h
+++ include/monday.h
@@ -1,7 +1,25 @@
-void fahrenheitCentigradeConversion(void) {
-	std::cout << " - fahrenheitCentigradeConversion: not yet implemented\n\n";
-}
+#include <ctype.h>
 
+void fahrenheitCentigradeConversion(void) {
+  using namespace std;
+
+  float temp  = 0.0;
+  char choice = '0';
+
+	cout << "Please enter the starting temperature: ";
+  cin >> temp;
+  cout << "Press 'C' to convert from Fahrenheit to Centigrade." << endl;
+  cout << "Press 'F' to convert from Centigrade to Fahrenheit." << endl;
+  cin >> choice;
+
+  if (tolower(choice) == 'c') {
+    cout << "";
+  } else if (tolower(choice) == 'f') {
+    cout << "";
+  } else {
+    cout << "Not a valid option";
+  }
+}
 
 void selfServiceCheckout(void) {
 	std::cout << " - selfServiceCheckout: not yet implemented\n\n";
