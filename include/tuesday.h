2f8eba48394047884618e9f6382684e062842aa7
ScottCrawley <74011247+Scott-Crawley@users.noreply.github.com>
2021-08-17 11:43:05 +0000

    Initial password strength calculation code

--- include/tuesday.h
+++ include/tuesday.h
@@ -1,5 +1,62 @@
+#include <ctype.h>
+
 void passwordComplexityChecker(void) {
-	std::cout << " - passwordComplexityChecker: not yet implemented\n\n";
+  std::string pwd;
+  std::string strength;
+
+	std::cout << "Enter a password: ";
+  std::cin >> pwd;
+
+  switch (calculatePasswordStrength(pwd)) {
+    case 1:
+      strength = "Weak";
+      break;
+    case 2:
+      strength = "Moderate";
+      break;
+    case 3:
+      strength = "Strong";
+      break;
+    case 4:
+      strength = "Very Strong";
+      break;
+    default:
+      strength = "U broke it";
+  }
+  std::cout << "The password '" << pwd << "' is " << strength;
+}
+
+int calculatePasswordStrength(std::string pwd) {
+  int length, alphabetic, digit, special;
+  length = pwd.length();
+
+  // <ctype.h> isalpha / isdigit
+  for (int i = 0; i < length; i++) {
+    char c = pwd[i];
+    if (isalpha(c)) {
+      alphabetic++;
+    }
+    else if (isdigit(c)) {
+      digit++;
+    }
+    else {
+      // Neither letter or number; it's a special char
+      special++;
+    }
+  }
+
+  // Determine strength
+  if (length >= 8 && digit >= 2 && alphabetic >= 4) {
+    // Ternary: no special chars = 'Strong', otherwise 'Very Strong'
+    return special != 0 ? 4 : 3;
+  }
+  if (length >= 4) {
+    // 'Moderate' if mixed, 'Weak' if same (length is equal to any singular type) 
+    if (!(alphabetic == length || digit == length || special == length)) {
+      return 2;
+    }
+  }
+  return 1;
 }
 
 
